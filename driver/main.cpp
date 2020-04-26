5851eff7f8ae4a3b47d24cac906ea9787ecadc1e
Peter Willemsen <peter@codebuffet.co>
2020-04-26 19:48:23 +0000

    commit before adding websocketpp

--- driver/main.cpp
+++ driver/main.cpp
@@ -2,6 +2,8 @@
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
 #include <math.h>
+#include <chrono>
+#include <thread>
 
 using namespace std;
 
@@ -16,26 +18,30 @@ int main(int, char **)
     int displayNumber = XDefaultScreen(d);
     
     Window rootWindow = XRootWindow(d, displayNumber);
+    Colormap colorMap = XDefaultColormap(d, displayNumber);
 
     const uint16_t gridW = floor(s->width / gridSize), gridH = floor(s->height / gridSize);
     const double totalPixelsMeasure = gridW * gridH;
-    double avgColorBufR = 0, avgColorBufG = 0, avgColorBufB = 0;
-
+    
     printf("Hodini Driver %s. Screen resolution is %dx%d. Split to grid %dx%d\n", VERSION, s->width, s->height, gridW, gridH);
-    for (uint16_t x = 0; x < gridW; x++)
-    {
-        for (uint16_t y = 0; y < gridH; y++)
+    while(true) {
+        double avgColorBufR = 0, avgColorBufG = 0, avgColorBufB = 0;
+        for (uint16_t x = 0; x < gridW; x++)
         {
-            XImage *image;
-            image = XGetImage(d, rootWindow, x * gridSize, y * gridSize, 1, 1, AllPlanes, ZPixmap);
-            c.pixel = XGetPixel(image, 0, 0);
-            XFree(image);
-            XQueryColor(d, XDefaultColormap(d, displayNumber), &c);
-            avgColorBufR += ((c.red / 256) / totalPixelsMeasure);
-            avgColorBufG += ((c.green / 256) / totalPixelsMeasure);
-            avgColorBufB += ((c.blue / 256) / totalPixelsMeasure);
+            for (uint16_t y = 0; y < gridH; y++)
+            {
+                XImage *image;
+                image = XGetImage(d, rootWindow, x * gridSize, y * gridSize, 1, 1, AllPlanes, ZPixmap);
+                c.pixel = XGetPixel(image, 0, 0);
+                XFree(image);
+                XQueryColor(d, colorMap, &c);
+                avgColorBufR += ((c.red / 256) / totalPixelsMeasure);
+                avgColorBufG += ((c.green / 256) / totalPixelsMeasure);
+                avgColorBufB += ((c.blue / 256) / totalPixelsMeasure);
+            }
         }
+        printf("Average color is: R: %f G: %f B: %f\n", avgColorBufR, avgColorBufG, avgColorBufB);
+        // std::this_thread::sleep_for(std::chrono::milliseconds(100));
     }
-    printf("Average color is: R: %f G: %f B: %f\n", avgColorBufR, avgColorBufG, avgColorBufB);
     return 0;
 }
